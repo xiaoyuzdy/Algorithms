7a8d7025dc16fd234db5eb58deb482675e4d8731
zcczdy <1104503035@qq.com>
2016-06-09 17:08:54 +0800

    16/6/9

--- AlgorithmsTest/src/Num1_3_01/Num_3_01_22.java
+++ AlgorithmsTest/src/Num1_3_01/Num_3_01_22.java
@@ -1,19 +1,19 @@
 package Num1_3_01;
 
 /**
- * P246T02
+ * P247 T22
  * 
  * @author he
  *
  */
 
-class ArrayST<Key extends Comparable<Key>, Value> {
+class ArrayST2<Key extends Comparable<Key>, Value> {
 	private Key keys[];
 	private Value values[];
 	private int N = 0;
 
 	@SuppressWarnings("unchecked")
-	public ArrayST(int size) {
+	public ArrayST2(int size) {
 		keys = (Key[]) new Comparable[size];
 		values = (Value[]) new Object[size];
 	}
@@ -39,23 +39,41 @@ class ArrayST<Key extends Comparable<Key>, Value> {
 	}
 
 	public Value get(Key key) {
-		if (select(key) != -1) {
-			return values[select(key)];
+		int i = select(key);
+		if (i != -1) {
+			Value v = values[i];
+			Key k = keys[i];
+
+			// 移动数组
+			for (int j = i; j > 0; j--) {
+				keys[j] = keys[j - 1];
+				values[j] = values[j - 1];
+			}
+			keys[0] = k;
+			values[0] = v;
+			return v;
 		}
 		return null;
+	}
+    //最近访问的键值对
+	public String recentlyGet() {
+		return "Key:" + keys[0] + " Value:" + values[0];
 	}
 
 }
 
-public class Num_3_01_02 {
+public class Num_3_01_22 {
 	public static void main(String[] args) {
-		ArrayST<String, Integer> st = new ArrayST<String, Integer>(10);
+		ArrayST2<String, Integer> st = new ArrayST2<String, Integer>(10);
 		st.put("A", 1);
 		st.put("B", 2);
 		st.put("A", 3);
 		st.put("X", 8);
-		System.out.println(st.get("X"));
+		
 		System.out.println(st.get("A"));
-	}
+		System.out.println(st.recentlyGet());
 
+		System.out.println(st.get("X"));
+		System.out.println(st.recentlyGet());
+	}
 }
