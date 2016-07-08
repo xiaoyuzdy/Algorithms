fc8a8e1756eb9bcebaa651041b956d507d954875
zcczdy <1104503035@qq.com>
2016-07-08 22:02:59 +0800

    16/7/8

--- AlgorithmsTest/src/Num1_3_03/Num_3_03_33.java
+++ AlgorithmsTest/src/Num1_3_03/Num_3_03_33.java
@@ -1,27 +1,23 @@
 package Num1_3_03;
 
 /**
- * P289 T29 以左结点大于右结点表示红连接,颜色变换就是交换左右连接 T30
- * 简单的做一些修改，如果put、get访问的结点是上一个访问结点则只需要常数时间
+ * P289 T32 T33
  * 
  * @author he
  *
  */
-
-class BRBST<Key extends Comparable<Key>, Value> {
-
+class BSTBR<Key extends Comparable<Key>, Value> {
 	private final static boolean RED = true;
 	private final static boolean BLACK = false;
 
 	private Node root;
-	private Node lately;// 用于保存最近访问的结点
 
 	private class Node {
-		private Key key;
-		private Value value;
-		private Node left, right;
-		private int N;
-		private boolean color;
+		Key key;
+		Value value;
+		Node left, right;
+		int N;
+		boolean color;
 
 		public Node(Key key, Value value, int N, boolean color) {
 			this.key = key;
@@ -42,28 +38,34 @@ class BRBST<Key extends Comparable<Key>, Value> {
 			return x.N;
 	}
 
-	private Node rotateLeft(Node h) {
-		Node t = h.right;
-		h.right = t.left;
-		t.left = h;
+	// 左旋转
+	private Node rotateLeft(Node x) {
+		Node t = x.right;
+		x.right = t.left;
+		t.left = x;
 
-		t.color = h.color;
-		h.color = RED;
+		t.color = x.color;
+		x.color = RED;
 
-		t.N = h.N;
-		h.N = size(h.left) + size(h.right) + 1;
+		t.N = x.N;
+		x.N = 1 + size(x.left) + size(x.right);
+
 		return t;
 	}
 
-	private Node rotateRight(Node h) {
-		Node t = h.left;
-		h.left = t.right;
-		t.right = h;
-		t.color = h.color;
-		h.color = RED;
-		t.N = h.N;
-		h.N = size(h.left) + size(h.right) + 1;
+	private Node rotateRight(Node x) {
+		Node t = x.left;
+		x.left = t.right;
+		t.right = x;
+
+		t.color = x.color;
+		x.color = RED;
+
+		t.N = x.N;
+		x.N = 1 + size(x.left) + size(x.right);
+
 		return t;
+
 	}
 
 	private boolean isRed(Node x) {
@@ -79,24 +81,20 @@ class BRBST<Key extends Comparable<Key>, Value> {
 
 	}
 
-	public boolean isEmpty() {
-		return root == null;
+	private Node min(Node x) {
+		if (x.left == null)
+			return x;
+		return min(x.left);
 	}
 
 	public void put(Key key, Value value) {
-		if (!isEmpty() && key.compareTo(lately.key) == 0) {
-			lately.value = value;
-			return;
-		}
-
 		root = put(root, key, value);
 		root.color = BLACK;
 	}
 
 	private Node put(Node x, Key key, Value value) {
 		if (x == null) {
-			lately = new Node(key, value, 1, RED);
-			return lately;
+			return new Node(key, value, 1, RED);
 		}
 
 		int cmp = key.compareTo(x.key);
@@ -104,10 +102,11 @@ class BRBST<Key extends Comparable<Key>, Value> {
 			x.left = put(x.left, key, value);
 		else if (cmp > 0)
 			x.right = put(x.right, key, value);
-		else
+		else {
 			x.value = value;
+		}
 
-		if (!isRed(x.left) && isRed(x.right))
+		if (isRed(x.right) && !isRed(x.left))
 			x = rotateLeft(x);
 		if (isRed(x.left) && isRed(x.left.left))
 			x = rotateRight(x);
@@ -120,9 +119,6 @@ class BRBST<Key extends Comparable<Key>, Value> {
 	}
 
 	public Value get(Key key) {
-		if (!isEmpty() && key.compareTo(lately.key) == 0) {
-			return lately.value;
-		}
 		return get(root, key);
 	}
 
@@ -136,31 +132,57 @@ class BRBST<Key extends Comparable<Key>, Value> {
 		else if (cmp > 0)
 			return get(x.right, key);
 		else
-			lately = x;// 保存最近访问的结点
-		return x.value;
+			return x.value;
 	}
 
-	public String lately() {
-		return "key: " + lately.key + " " + "value: " + lately.value;
+	public boolean is23() {
+		return is23(root);
+	}
+
+	private boolean is23(Node x) {
+		if (x == null)
+			return true;
+		if (isRed(x.right))
+			return false;
+		if (x != root && isRed(x) && isRed(x.left))
+			return false;
+		return is23(x.left) && is23(x.right);
+	}
+
+	public boolean isBalanced() {
+		int black = 0;// 黑连接数
+		Node x = root;
+		if (!isRed(x)) {
+			black++;
+			x = x.left;
+		}
+
+		return isBalanced(x, black);
+
+	}
+
+	private boolean isBalanced(Node x, int black) {
+		if (x == null)
+			return black == 0;
+		if (!isRed(x))
+			black--;
+		return isBalanced(x.left, black) && isBalanced(x.right, black);
 	}
 
 }
 
-public class Num_3_03_30 {
+public class Num_3_03_33 {
 	public static void main(String[] args) {
-		BRBST<String, Integer> b = new BRBST<String, Integer>();
+		BSTBR<String, Integer> b = new BSTBR<String, Integer>();
 		b.put("A", 1);
 		b.put("A", 2);
 		b.put("B", 3);
 		b.put("C", 4);
-		b.put("B", 8);
-		b.put("D", 5);
-		System.out.println(b.get("C"));
-		b.put("C", 7);
-		System.out.println(b.lately());
-		System.out.println(b.get("C"));
-		System.out.println(b.get("A"));
-		System.out.println(b.get("B"));
+		b.put("E", 5);
+		b.put("D", 7);
+		System.out.println(b.is23());
+		System.out.println(b.isBalanced());
+
 	}
 
 }
