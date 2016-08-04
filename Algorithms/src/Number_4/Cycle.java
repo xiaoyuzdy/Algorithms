994089d02229b542081e44ba95faa56edb1ed518
zcczdy <1104503035@qq.com>
2016-08-04 15:40:39 +0800

    16/8/4

---
+++ Algorithms/src/Number_4/Cycle.java
@@ -0,0 +1,124 @@
+package Number_4;
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
+/**
+ * 检测图是否有环，可包含自环，平行边
+ * args[0]:tinyCG.txt
+ * 
+ * @author he
+ *
+ */
+public class Cycle {
+	private boolean marked[];
+	private int edgeTo[];// 用于保存环
+	private Stack<Integer> path;
+
+	public Cycle(Graph G) {
+		if (hasSelfLoop(G))
+			return;
+		if (hasParallelEdges(G))
+			return;
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		for (int v = 0; v < G.V(); v++) {
+			if (!marked[v])
+				dfs(G, v, v);
+		}
+
+	}
+
+	/**
+	 * 判断是否含有自环
+	 * 
+	 * @param G
+	 * @return
+	 */
+	private boolean hasSelfLoop(Graph G) {
+		for (int v = 0; v < G.V(); v++) {
+			for (int w : G.adj(v)) {
+				if (w == v) {
+					path = new Stack<Integer>();
+					path.push(w);
+					path.push(v);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 判断是否含有平行边
+	 * 
+	 * @param G
+	 * @return
+	 */
+	private boolean hasParallelEdges(Graph G) {
+		marked = new boolean[G.V()];
+		for (int i = 0; i < G.V(); i++) {
+			for (int w : G.adj(i)) {
+				// 平行边，顶点i有两条边到达顶点w，如果一个顶点已经被标记过则表明至少有两条边到达同一顶点
+				if (marked[w]) {
+					path = new Stack<Integer>();
+					path.push(i);
+					path.push(w);
+					path.push(i);
+					return true;
+				}
+				marked[w] = true;
+			}
+
+			// 重置marked数组,让其恢复至初始状态
+			for (int w : G.adj(i))
+				marked[w] = false;
+
+		}
+		return false;
+	}
+
+	private void dfs(Graph G, int u, int v) {
+		marked[v] = true;
+
+		for (int w : G.adj(v)) {
+			// 已发现环
+			if (path != null)
+				return;
+			if (!marked[w]) {
+				marked[w] = true;
+				edgeTo[w] = v;
+				dfs(G, v, w);
+			}
+			// 因为这是深度遍历，所以如果可以通过两个顶点递归遍历到v则认为有环
+			else if (w != u) {
+				path = new Stack<Integer>();
+				for (int x = v; x != w; x = edgeTo[x]) {
+					path.push(x);
+				}
+				path.push(w);
+				path.push(v);
+			}
+		}
+	}
+
+	public Iterable<Integer> path() {
+		return path;
+	}
+
+	public boolean hasCycle() {
+		return path != null;
+	}
+
+	public static void main(String[] args) {
+		Graph G = new Graph(new In(args[0]));
+		Cycle cycle = new Cycle(G);
+		if (cycle.hasCycle()) {
+			for (int i : cycle.path()) {
+				System.out.print(" " + i);// 1 0 2 1
+			}
+		}
+	}
+
+}
---
+++ Algorithms/src/Number_4/Digraph.java
@@ -0,0 +1,101 @@
+package Number_4;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+/**
+ * P366 Digraph（有向图）数据类型 代码和无向图基本类似，只是在添加边时无需添加两次 同时加入了有向图取反，这样可以得到指向每个顶点的所有的边
+ * 
+ * @author he
+ *
+ */
+public class Digraph {
+
+	private final int V;// 有向图的顶点个数
+	private int E;// 边的个数
+	private Bag<Integer> adj[];// 邻接表
+
+	@SuppressWarnings("unchecked")
+	public Digraph(int v) {
+		this.V = v;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int i = 0; i < V; i++) {
+			adj[i] = new Bag<Integer>();
+		}
+	}
+
+	public Digraph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			addEdge(v, w);
+		}
+	}
+
+	/**
+	 * 添加边
+	 * 
+	 * @param v
+	 * @param w
+	 */
+	public void addEdge(int v, int w) {
+		adj[v].add(w);
+		E++;
+	}
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	/**
+	 * 返回所有顶点v指向的顶点
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+	/**
+	 * 有向图的反向图，可得到所有指向顶点的边
+	 * 
+	 * @return
+	 */
+	public Digraph reverse() {
+		Digraph d = new Digraph(V);
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v))
+				d.addEdge(w, v);
+		}
+		return d;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append("有" + V + "个顶点" + "   " + E + "条边" + "\n");
+		for (int v = 0; v < V; v++) {
+			s.append(v + ": ");
+			for (int w : adj(v))
+				s.append(w + " ");
+			s.append("\n");
+		}
+		return s.toString();
+	}
+
+	public static void main(String[] args) {
+		Digraph dg = new Digraph(new In(args[0]));
+		System.out.println(dg.toString());
+		Digraph gd = dg.reverse();
+		System.out.println(gd.toString());
+	}
+
+}
