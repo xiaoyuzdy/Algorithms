fdd02cddddc243e442bf5cdd11666036db32397e
zcczdy <1104503035@qq.com>
2016-08-09 20:48:22 +0800

    16/8/9

--- AlgorithmsTest2/src/Num2_4_02/Num_4_02_21.java
+++ AlgorithmsTest2/src/Num2_4_02/Num_4_02_21.java
@@ -3,75 +3,67 @@ package Num2_4_02;
 import java.util.LinkedList;
 import java.util.List;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Queue;
 
 /**
- * P387 T21 
- * 顶点v和w的共同祖先，即起点到v和起点到w的路径中离指定顶点最近且相同的顶点 (图G没有环) 
- * 顶点的输入请按规定输入，在这不做判断
+ * P387 T21 顶点的输入请按规定输入，在这不做判断
  * 
  * @author he
  *
  */
 
 class LAC {
+	private Digraph G;// 图G的反向图
 	private boolean marked[];
-	private int edgeTo[];
-	private int s;// 起点
 
-	public LAC(Digraph G, int s) {
-		marked = new boolean[G.V()];
-		edgeTo = new int[G.V()];
-		this.s = s;
-		dfs(G, s);
+	public LAC(Digraph G) {
+		this.G = G.reverse();
 	}
 
-	// 可达性分析
-	private void dfs(Digraph G, int v) {
+	/**
+	 * 在逆向图G中顶点v
+	 * 
+	 * 
+	 * 因为使用广度优先搜索，所以linkedlist保存的顺序就是顶点v指向的最近的顶点的顺序
+	 * 
+	 * @param v
+	 */
+	public Iterable<Integer> find(Digraph G, int v) {
+		marked = new boolean[G.V()];// 重置
+		Queue<Integer> queue = new Queue<Integer>();
+		List<Integer> l = new LinkedList<Integer>();
 		marked[v] = true;
-		for (int w : G.adj(v))
-			if (!marked[w]) {
-				edgeTo[w] = v;
-				dfs(G, w);
+		queue.enqueue(v);
+		while (!queue.isEmpty()) {
+			int t = queue.dequeue();
+			for (int w : G.adj(t)) {
+				if (!marked[w]) {
+					l.add(w);
+					marked[w] = true;
+					queue.enqueue(w);
+				}
 			}
-	}
-
-	private boolean hasPath(int v) {
-		return marked[v];
-	}
-
-	/**
-	 * 指定顶点到起点的路径
-	 * 
-	 * @return
-	 */
-	private Iterable<Integer> pathTo(int v) {
-		if (!hasPath(v))
-			return null;
-		List<Integer> list = new LinkedList<Integer>();
-		for (int x = v; x != s; x = edgeTo[x]) {
-			list.add(x);
 		}
-		list.add(s);
-		// 保存的路径已起点结尾
-		return list;
+		return l;
 	}
 
 	/**
-	 * 查找最近共同祖先
+	 * 找出顶点v和顶点w的最近共同祖先
 	 * 
+	 * @param v
+	 * @param w
 	 * @return
 	 */
-	public int findLCA(int v, int w) {
-		if (!hasPath(v) || !hasPath(w))
-			throw new RuntimeException("v和 w没有共同祖先");
-		List<Integer> lv = (List<Integer>) pathTo(v);
-		List<Integer> lw = (List<Integer>) pathTo(w);
+	public int findLAC(int v, int w) {
+		List<Integer> lv = (List<Integer>) find(G, v);
+		List<Integer> lw = (List<Integer>) find(G, w);
 		if (lv.size() > lw.size()) {
 			for (int t : lw) {
 				if (lv.contains(t))
 					return t;
 			}
 		} else {
+
 			for (int t : lv) {
 				if (lw.contains(t))
 					return t;
@@ -84,13 +76,16 @@ class LAC {
 
 public class Num_4_02_21 {
 	public static void main(String[] args) {
-		Digraph G = new Digraph(5);
-		G.addEdge(0, 2);
+		Digraph G = new Digraph(6);
+		G.addEdge(2, 3);
 		G.addEdge(0, 1);
+		G.addEdge(0, 2);
 		G.addEdge(1, 4);
 		G.addEdge(1, 3);
-		LAC lac = new LAC(G, 0);
-		System.out.println(lac.findLCA(3, 4));// 1
+		G.addEdge(3, 5);
+		LAC lac = new LAC(G);
+		System.out.println(lac.findLAC(3, 4));
+
 	}
 
 }
