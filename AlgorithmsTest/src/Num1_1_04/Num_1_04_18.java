d0559e5f3782336400ee8fd9621fa76d42d79971
zcczdy <1104503035@qq.com>
2016-09-29 16:08:27 +0800

    c

--- AlgorithmsTest/src/Num1_1_04/Num_1_04_18.java
+++ AlgorithmsTest/src/Num1_1_04/Num_1_04_18.java
@@ -33,10 +33,13 @@ public class Num_1_04_18 {
 				return i;// 命中
 			// 上坡
 			if (a[i - 1] < a[i] && a[i] < a[i + 1]) {
-				if (i - 2 >= 0 && a[i - 2] >= a[i])
+				if (i - 2 >= 0 && a[i - 2] > a[i - 1])
 					return i - 1;
-				else if (i - 2 >= 0) {
+				// 跳到非顶点元素
+				else if (i - 2 > 0) {
 					i = i - 2;
+				} else {
+					i--;
 				}
 				// 移动到最左边还没有发现目标元素，此时扫描右边
 				if (i <= 1 && temp <= 1) {
@@ -50,10 +53,13 @@ public class Num_1_04_18 {
 
 			// 下坡
 			if (a[i - 1] > a[i] && a[i] > a[i + 1]) {
-				if (i + 2 < a.length && a[i + 2] >= a[i])
+				if (i + 2 < a.length && a[i + 2] > a[i + 1])
 					return i + 1;
-				else if (i + 2 < a.length) {
+				// 跳到非顶点元素
+				else if (i + 2 < a.length - 1) {
 					i = i + 2;
+				} else {
+					i++;
 				}
 
 				// 移动到最右边还没有发现目标元素，此时扫描左边
@@ -83,13 +89,13 @@ public class Num_1_04_18 {
 		int a2[] = { 7, 6, 5, 4, 3, 0, 2, 1 };
 		int a3[] = { 1, 6, 7, 3, 5, 6, 4, 2 };
 		int a4[] = { 1, 2, 3, 4, 5, 6 };
-		int a5[] = { 5, 4, 3, 2, 1 };
+		int a5[] = { 5, 0, 3, 2, 1 };
 
 		int a6[] = new int[50];
 		for (int i = 0; i < a6.length; i++)
 			a6[i] = i;
 		StdRandom.shuffle(a6);
-		System.out.println(minElement(a3));
+		System.out.println(minElement(a6));
 	}
 
 }
