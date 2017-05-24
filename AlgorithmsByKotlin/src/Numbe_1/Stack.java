76ef2cf4e63c319b27cf88faf4177d6227ffd3e4
zcczdy <1104503035@qq.com>
2017-05-24 16:26:01 +0800

    commit

---
+++ AlgorithmsByKotlin/src/Numbe_1/Stack.java
@@ -0,0 +1,78 @@
+package Numbe_1;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * 使用链表实现下压栈 在头结点实现push() 和pop()操作
+ *
+ * @author he
+ */
+public class Stack<Item> implements Iterable<Item> {
+    private Node first;// 头结点
+    private int N; // 栈中元素个数
+    // 链表的结点
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Iterator<Item> iterator() {
+        // TODO Auto-generated method stub
+        return new listIterator();
+    }
+
+    private class listIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+
+            return current != null;
+        }
+
+        // 获取头结点
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        for (String string : args) {
+            if (!string.equals("-")) {
+                stack.push(string);
+            } else if (!stack.isEmpty()) {
+                System.out.print(stack.pop() + " ");
+            }
+        }
+        System.out.println("(" + stack.size() + " left on stack" + ")");
+    }
+
+}
---
+++ AlgorithmsByKotlin/src/Numbe_1/Stack.kt
@@ -0,0 +1,18 @@
+package Numbe_1
+
+/**
+ * Created by he on 2017/5/24.
+ */
+
+typealias StackKt = Numbe_1.Stack<Any>
+
+fun main(args: Array<String>) {
+    val stack = StackKt()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    for (i in stack){
+        println("  "+i)
+    }
+}
+
